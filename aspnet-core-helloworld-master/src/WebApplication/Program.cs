bdcf9f5ef02920f6edf4aa2ceb023c9de12ebae8
OceanicRavi <18ravibhatt@gmail.com>
2017-06-09 10:15:43 +1200

    Starter code added by ravi

---
+++ aspnet-core-helloworld-master/src/WebApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                        .UseKestrel()
+                        .UseStartup<Startup>()
+                        .Build();
+            host.Run();
+        }
+    }
+}
